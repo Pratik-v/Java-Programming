66c5b674ea63c47e4e59bccac1965a1cea8839bf
PRIYANSHU VYAS <84512213+Priyanshu-Vyas@users.noreply.github.com>
2021-12-26 20:51:02 +0530

    Create AWT-MouseEvent.java

---
+++ AWT-MouseEvent.java
@@ -0,0 +1,39 @@
+import java.awt.*; 
+import java.awt.event.MouseListener; 
+import java.awt.event.MouseEvent; 
+class ExitExp extends Exception{ 
+  ExitExp(){ 
+    System.out.println("Exception Triggered"); 
+  } 
+} 
+class ML implements MouseListener { 
+  Frame f; Label l; 
+  ML() { 
+    f=new Frame(); 
+    f.setSize(500,500); 
+    f.addMouseListener(this); 
+    f.setVisible(true); 
+  } 
+  void Exp()throws ExitExp{
+    throw new ExitExp();
+  } 
+  public void mouseClicked(MouseEvent e) { 
+    System.out.println("Mouse Clicked"); 
+  } 
+  public void mouseExited(MouseEvent e) { 
+    System.out.println("Mouse Exited"); 
+    try{Exp();} catch(ExitExp m){} 
+  } 
+  public void mousePressed(MouseEvent e) { 
+    System.out.println("Mouse Pressed"); 
+  } 
+  public void mouseReleased(MouseEvent e) { 
+    System.out.println("Mouse Released");
+    } 
+  public void mouseEntered(MouseEvent e) { 
+    System.out.println("Mouse Entered"); 
+  } 
+  public static void main(String args[]) { 
+    ML m=new ML(); 
+  } 
+}
